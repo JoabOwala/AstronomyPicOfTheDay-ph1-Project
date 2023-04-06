5a81259fd8144db64a9edd1e8aeb4ad9eb84967f
Joab Owala <owalajoab@gmail.com>
2023-04-06 13:10:45 +0300

    added like emoji

--- src/index.js
+++ src/index.js
@@ -1,31 +1,41 @@
 document.addEventListener('DOMContentLoaded', () =>{
-
+    
     console.log('DOM IS READY')
 // Defining characters for the empty and full hearts.
-const fullHeart = '<i id="heart2" class="fa-solid fa-heart fa-2xl" style="color: #ff0000;"></i>'
-const emptyHeart = '<i id="heart1" class="fa-regular fa-heart fa-2xl" style="color: #ff0000;"></i>'
+const fullHeart = '♥'
+const emptyHeart = '♡'
 const baseURL = `https://api.nasa.gov/planetary/apod?api_key=neIf881jkrVVBuY8LMMlAKE1DgCukaoc0JS6GZoU`
+const finalImg = document.querySelector('#image')
+const finalTitle = document.querySelector('.title')
+const finalDate = document.querySelector('.date')
+const finalDescription = document.querySelector('.description')
 
-
+//retrive data from API
 fetch(baseURL)
 .then(res => res.json())
 .then(data => renderData(data))
 
-const upperContainer = document.querySelector('.container')
 function renderData(data){
-    const finalImg = document.querySelector('#image')
     finalImg.src = `${data.url}`
     finalImg.alt = `${data.title}`
-
-    const finalTitle = document.querySelector('.title')
     finalTitle.innerHTML = `${data.title}`
-    const finalDate = document.querySelector('.date')
     finalDate.innerHTML = `${data.date}`
-    const finalDescription = document.querySelector('.description')
-    finalDescription.innerHTML = `${data.explanation}`
-
-    //upperContainer.appendChild(finalImg)
-    
+    finalDescription.innerHTML = `${data.explanation}`    
 }
+const myHeartClick = document.querySelectorAll("#heart")
+
+function heartCallback(e){
+    const heartEmoji = e.target
+
+    if(heartEmoji.innerHTML === emptyHeart){
+        heartEmoji.innerHTML = fullHeart
+    }else{
+        heartEmoji.innerHTML = emptyHeart
+    }
+}
+
+for(const emoji of myHeartClick){
+    emoji.addEventListener("click", heartCallback)
+  }
 
 })
