cb5ab996d86eedad85a10c21792cf81be45b3fe9
Joab Owala <owalajoab@gmail.com>
2023-04-06 10:58:36 +0300

    Added heart emoji for the like feature

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// Defining characters for the empty and full hearts.
+const fullHeart = <i class="fa-solid fa-heart fa-2xl" style="color: #ff0000;"></i> 
+const emptyHeart = <i id="heart" class="fa-regular fa-heart fa-2xl" style="color: #ff0000;"></i>
