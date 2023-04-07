ced59c63c454a65475d647e9c020cc6a4191d868
Joab Owala <owalajoab@gmail.com>
2023-04-07 11:32:39 +0300

    Like button count and functionality

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ const apodImage = document.getElementById('apod-image');
 const apodTitle = document.getElementById('apod-title');
 const apodExplanation = document.getElementById('apod-explanation');
 const apodDate = document.getElementById('apod-date');
+const likeButton = document.getElementById('like-button');
+const likeCount = document.getElementById('like-count');
 
 // Fetch Astronomy Picture of the Day from API
 fetch('https://api.nasa.gov/planetary/apod?api_key=neIf881jkrVVBuY8LMMlAKE1DgCukaoc0JS6GZoU')
   .then(response => response.json())
   .then(data => {
+
     // Set image source, title, explanation and date
     apodImage.src = data.hdurl;
     apodTitle.textContent = data.title;
@@ -17,5 +20,12 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=neIf881jkrVVBuY8LMMlAKE1DgCuk
     apodDate.textContent = data.date;
   })
   .catch(error => console.error(error));
-  
+
+  // Add event listener to like button
+likeButton.addEventListener('click', () => {
+  let count = parseInt(likeCount.textContent);
+  count++;
+  likeCount.textContent = count;
+});
+
 })
